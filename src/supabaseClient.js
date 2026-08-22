a20337c0236463dcb16b4054f953ba4a16af4f13
Dieulin80 <hypernetflix04@gmail.com>
2026-08-22 14:33:32 -0400

    Update Supabase URL in supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
+// supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wepevdfxxihcqwmektnt.supabase.co";
+const SUPABASE_URL = "https://hsbifpngubxfkmypkjxn.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_88P3M5z86-tvAfD4WYpy9g_5zEyZzYM";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
